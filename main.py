4d254a1ce5a1a3d05c575fadd97f78d1ba3403c2
Sakar Pathak <075bei030.sakar@pcampus.edu.np>
2021-07-16 09:34:16 +0545

    this is the first commit of flask_bokeh v1.0

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+from flask import Flask, request, render_template, abort, Response
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    x = np.arange(2, 50, step=.5)
+    y = np.sqrt(x) + np.random.randint(2,50)
+    plot = figure(plot_width=400, plot_height=400,title=None, toolbar_location="below")
+    plot.line(x,y)
+
+    script, div = components(plot)
+
+    return render_template('index.html', title = 'swjayam dhak', script = script, div = div)
+
+if __name__ == '__main__':
+    app.run(debug=True)
